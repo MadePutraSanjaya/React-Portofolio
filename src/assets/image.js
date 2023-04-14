7684ca45d31eb2cf91fa17a2573476b47da18e8d
MadePutraSanjaya <madeputra.6166@gmail.com>
2023-04-14 18:26:05 +0800

    .

--- src/assets/image.js
+++ src/assets/image.js
@@ -5,5 +5,6 @@ const porto3 = require('./porto3.png')
 const porto4 = require('./porto4.png')
 const porto5 = require('./porto5.png')
 const porto6 = require('./porto6.png')
+const porto7 = require("./porto7.png");
 
-export {porto1, porto2, porto3, porto4 , porto5, porto6}
+export {porto1, porto2, porto3, porto4 , porto5, porto6, porto7}
